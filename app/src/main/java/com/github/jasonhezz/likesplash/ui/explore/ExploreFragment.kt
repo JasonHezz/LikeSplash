d866b3841b6ef8b0d552716a3b20b249668eef73
JasonHezz <mnihuzhe@163.com>
2017-12-05 18:03:20 +0800

    [feature]explore page

--- app/src/main/java/com/github/jasonhezz/likesplash/ui/explore/ExploreFragment.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/explore/ExploreFragment.kt
@@ -5,10 +5,14 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
 import com.github.jasonhezz.likesplash.R
+import com.github.jasonhezz.likesplash.ui.MainActivity
+import kotlinx.android.synthetic.main.fragment_explore.*
 
 class ExploreFragment : Fragment() {
 
+  val items = listOf("Business", "Girl", "Nature", "Technology", "Food", "Travel", "Happy", "Cool")
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -25,6 +29,10 @@ class ExploreFragment : Fragment() {
 
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
+    toolbar.setNavigationOnClickListener { if (activity is MainActivity) (activity as MainActivity).openDrawer() }
+    val adapter = ArrayAdapter(context, R.layout.toolbar_title, items)
+    adapter.setDropDownViewResource(android.R.layout.simple_list_item_1)
+    spinner_nav.adapter = adapter
   }
 
   companion object {
