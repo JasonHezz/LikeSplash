ad925d34961b240b78540c7285925f345426ed39
JasonHezz <mnihuzhe@163.com>
2017-12-12 18:06:08 +0800

    [feature]paging library test

--- app/src/main/java/com/github/jasonhezz/likesplash/data/api/Resource.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/data/api/Resource.kt
@@ -3,4 +3,10 @@ package com.github.jasonhezz.likesplash.data.api
 /**
  * Created by JavaCoder on 2017/11/28.
  */
-data class Resource(val status: Status, val message: String? = null)
+data class Resource(val status: Status, val message: String? = null) {
+  companion object {
+    val LOADED = Resource(Status.SUCCESS)
+    val LOADING = Resource(Status.RUNNING)
+    fun error(msg: String?) = Resource(Status.ERROR, msg)
+  }
+}
---
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/collection/CollectionViewModel.kt
@@ -0,0 +1,10 @@
+package com.github.jasonhezz.likesplash.ui.collection
+
+import android.arch.lifecycle.ViewModel
+
+/**
+ * Created by JavaCoder on 2017/12/12.
+ */
+class CollectionViewModel: ViewModel(){
+
+}
