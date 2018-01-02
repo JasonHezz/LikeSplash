8c9222804cee5447eb7841f239ce8fd783f91979
JasonHezz <mnihuzhe@163.com>
2018-01-02 11:07:45 +0800

    [refactor]tweak code

--- app/src/main/java/com/github/jasonhezz/likesplash/ui/timeline/TimelineViewModel.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/timeline/TimelineViewModel.kt
@@ -2,27 +2,22 @@ package com.github.jasonhezz.likesplash.ui.timeline
 
 import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Transformations
+import android.arch.lifecycle.ViewModel
 import com.github.jasonhezz.likesplash.data.Photo
 import com.github.jasonhezz.likesplash.repository.Listing
 import com.github.jasonhezz.likesplash.repository.PhotoRepository
-import com.github.jasonhezz.likesplash.ui.RxAwareViewModel
 
 /**
  * Created by JavaCoder on 2017/11/27.
  */
-class TimelineViewModel(private val repository: PhotoRepository) : RxAwareViewModel() {
+class TimelineViewModel(private val repository: PhotoRepository) : ViewModel() {
 
-  /* var refreshOnStartup: Boolean = true
-   private val nextPage = MutableLiveData<Int>().apply { postValue(1) }
-   val messages = MutableLiveData<Resource>()
-   val photos = MutableLiveData<List<Photo>>().apply { postValue(emptyList()) }*/
   private val result = MutableLiveData<Listing<Photo>>()
   val photos = Transformations.switchMap(result, { it.pagedList })!!
   val networkState = Transformations.switchMap(result, { it.networkState })!!
   val refreshState = Transformations.switchMap(result, { it.refreshState })!!
 
   init {
-    // Eagerly refresh the initial page of trending
     result.postValue(repository.getListPhotos())
   }
 
