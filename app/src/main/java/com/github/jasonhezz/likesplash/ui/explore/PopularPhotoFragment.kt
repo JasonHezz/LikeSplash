2579a35434eae9ac00462c4a40f4189a19df7a95
JasonHezz <mnihuzhe@163.com>
2018-01-10 16:26:39 +0800

    [feature]popular photo custom decoration

--- app/src/main/java/com/github/jasonhezz/likesplash/ui/explore/PopularPhotoFragment.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/explore/PopularPhotoFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.jasonhezz.likesplash.R
 import com.github.jasonhezz.likesplash.ui.controller.ExploreController
+import com.github.jasonhezz.likesplash.util.recyclerview.PopularPhotoDecoration
 import kotlinx.android.synthetic.main.fragment_popular_photo.*
 
 class PopularPhotoFragment : Fragment() {
@@ -29,7 +30,7 @@ class PopularPhotoFragment : Fragment() {
   override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
     super.onViewCreated(view, savedInstanceState)
     rv.setControllerAndBuildModels(controller)
-    rv.isNestedScrollingEnabled = false
+    rv.addItemDecoration(PopularPhotoDecoration(context!!))
   }
 
   companion object {
