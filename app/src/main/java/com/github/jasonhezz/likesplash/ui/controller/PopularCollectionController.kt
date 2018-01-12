e5153c8dcc2a38430c77c0d40ae3624b42118088
JasonHezz <mnihuzhe@163.com>
2018-01-12 15:25:42 +0800

    [feature]popular collection tablet mode

--- app/src/main/java/com/github/jasonhezz/likesplash/ui/controller/PopularCollectionController.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/controller/PopularCollectionController.kt
@@ -1,15 +1,18 @@
 package com.github.jasonhezz.likesplash.ui.controller
 
+import android.content.Context
 import com.airbnb.epoxy.EpoxyController
+import com.github.jasonhezz.likesplash.R
 import com.github.jasonhezz.likesplash.data.ExploreCollection
 import com.github.jasonhezz.likesplash.data.model.exploreCollection
 import com.github.jasonhezz.likesplash.data.model.exploreCollectionThumb
 import com.github.jasonhezz.likesplash.data.model.exploreTitle
+import com.github.jasonhezz.likesplash.util.recyclerview.NumItemsInGridRow
 
 /**
  * Created by JavaCoder on 2018/1/10.
  */
-class PopularCollectionController() : EpoxyController() {
+class PopularCollectionController(val context: Context) : EpoxyController() {
 
   var explores = emptyList<ExploreCollection>()
     set(value) {
@@ -29,11 +32,14 @@ class PopularCollectionController() : EpoxyController() {
           exploreCollection {
             id(collection.id)
             collection(collection)
+            spanSizeOverride(NumItemsInGridRow(context, R.integer.grid_explore_collection_per_row))
           }
         } else {
           exploreCollectionThumb {
             id(collection.id)
             collection(collection)
+            spanSizeOverride(
+                NumItemsInGridRow(context, R.integer.grid_explore_collection_thumb_per_row))
           }
         }
       }
