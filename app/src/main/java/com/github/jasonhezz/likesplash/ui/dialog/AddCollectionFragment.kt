eedd1a63acbedefd8548cbf0a3dca9497a034d37
JasonHezz <mnihuzhe@163.com>
2018-01-16 18:02:55 +0800

    [feature]new collection

--- app/src/main/java/com/github/jasonhezz/likesplash/ui/dialog/AddCollectionFragment.kt
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/dialog/AddCollectionFragment.kt
@@ -8,36 +8,29 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.github.jasonhezz.likesplash.R
-import com.github.jasonhezz.likesplash.ui.Adapter
-import com.github.jasonhezz.likesplash.ui.ListFragment
 
 /**
  * Created by JasonHezz on 2018/1/15.
 
  */
 class AddCollectionFragment : DialogFragment() {
-    val adpater = Adapter(ListFragment.generateItemModelList())
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.dialog_search, container, false)
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    dialog.window.decorView.setPadding(0, 0, 0, 0)
+    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+//    dialog.window.attributes.windowAnimations = R.style.WindowAnimation_Grow
+    dialog.window.setLayout(900, 560)
+    return inflater.inflate(R.layout.dialog_search, container, false)
+  }
+
+  companion object {
+    @JvmStatic
+    fun newInstance(): AddCollectionFragment {
+      val fragment = AddCollectionFragment()
+      val args = Bundle()
+      fragment.arguments = args
+      return fragment
     }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        if (getDialog() != null && getDialog().getWindow() != null) {
-            getDialog().getWindow().setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
-//        rv.adapter = adpater
-    }
-
-
-    companion object {
-        @JvmStatic
-        fun newInstance(): AddCollectionFragment {
-            val fragment = AddCollectionFragment()
-            val args = Bundle()
-            fragment.arguments = args
-            return fragment
-        }
-    }
+  }
 }
