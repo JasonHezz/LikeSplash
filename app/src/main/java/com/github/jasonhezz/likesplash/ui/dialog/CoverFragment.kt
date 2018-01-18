98e407e79da4ac455812aa564c8d5537f3df2d32
JasonHezz <mnihuzhe@163.com>
2018-01-18 18:10:14 +0800

    [feature]slide anim, cover dialog

---
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/dialog/CoverFragment.kt
@@ -0,0 +1,79 @@
+package com.github.jasonhezz.likesplash.ui.dialog
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.jasonhezz.likesplash.R
+import com.github.jasonhezz.likesplash.data.Collection
+import com.github.jasonhezz.likesplash.data.Photo
+import com.github.jasonhezz.likesplash.util.glide.GlideApp
+import kotlinx.android.synthetic.main.dialog_add_cover.*
+
+/**
+ * Created by JavaCoder on 2018/1/18.
+ */
+class CoverFragment : DialogFragment(), AddCollectionFragment.Companion.Callbacks, CreateCollectionFragment.Companion.Callbacks {
+
+  private var photo: Photo? = null
+  private var data: List<Collection>? = null
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    if (arguments != null) {
+      photo = arguments?.getParcelable(ARG_PARAM_PHOTO)
+      data = arguments?.getParcelableArrayList(ARG_PARAM_COLLECTION)
+    }
+  }
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    dialog.window.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    return inflater.inflate(R.layout.dialog_add_cover, container, false)
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    GlideApp.with(context).load(photo?.urls?.regular)
+        .into(cover)
+    if (savedInstanceState == null) {
+      childFragmentManager.beginTransaction().add(R.id.action_panel,
+          AddCollectionFragment.newInstance(photo!!, data as ArrayList<Collection>)).commit()
+    }
+  }
+
+  override fun onCollectionClick() {
+    //    https@ //issuetracker.google.com/issues/37036000
+    childFragmentManager.beginTransaction().setCustomAnimations(R.anim.slide_in_right,
+        0, 0, android.R.anim.slide_out_right).replace(
+        R.id.action_panel,
+        CreateCollectionFragment.newInstance()).addToBackStack(null).commit()
+  }
+
+  override fun onCreateButtonClick() {
+
+  }
+
+  override fun onCancelButtonClick() {
+    childFragmentManager.popBackStack()
+  }
+
+  companion object {
+
+    private const val ARG_PARAM_COLLECTION = "collection"
+    private const val ARG_PARAM_PHOTO = "photo"
+
+    @JvmStatic
+    fun newInstance(photo: Photo, data: ArrayList<Collection>? = null): CoverFragment {
+      val fragment = CoverFragment()
+      val args = Bundle()
+      args.putSerializable(ARG_PARAM_PHOTO, photo)
+      args.putParcelableArrayList(ARG_PARAM_COLLECTION, data)
+      fragment.arguments = args
+      return fragment
+    }
+  }
+}
