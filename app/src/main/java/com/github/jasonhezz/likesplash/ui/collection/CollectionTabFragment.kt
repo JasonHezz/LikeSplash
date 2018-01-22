96ed58a7ddf49006f6382b7a5f4cae5b2f65197f
JasonHezz <mnihuzhe@163.com>
2018-01-22 17:53:31 +0800

    [refactor]tablayout replace bottomnavigation

---
+++ app/src/main/java/com/github/jasonhezz/likesplash/ui/collection/CollectionTabFragment.kt
@@ -0,0 +1,54 @@
+package com.github.jasonhezz.likesplash.ui.collection
+
+import android.os.Bundle
+import android.support.design.widget.TabLayout
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.github.jasonhezz.likesplash.R
+import com.github.jasonhezz.likesplash.ui.MainActivity
+import com.github.jasonhezz.unofficialsplash.home.TabFragmentAdapter
+import kotlinx.android.synthetic.main.fragment_home.*
+
+
+/**
+ * Created by JasonHezz on 2017/10/15.
+ */
+class CollectionTabFragment : Fragment() {
+
+  private lateinit var tabAdapter: TabFragmentAdapter
+
+  override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+      savedInstanceState: Bundle?): View? {
+    return inflater.inflate(R.layout.fragment_collection_tab, container, false)
+  }
+
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+    initToolbar()
+    initViewPager()
+  }
+
+  private fun initToolbar() {
+    toolbar.setNavigationOnClickListener { if (activity is MainActivity) (activity as MainActivity).openDrawer() }
+  }
+
+  private fun initViewPager() {
+    tabAdapter = TabFragmentAdapter(childFragmentManager)
+    tabAdapter.addFragment(CollectionFragment.newInstance())
+    tabAdapter.addFragment(CollectionFragment.newInstance())
+    view_pager.apply {
+      adapter = tabAdapter
+      addOnPageChangeListener(TabLayout.TabLayoutOnPageChangeListener(tab_layout))
+    }
+    tab_layout?.addOnTabSelectedListener(TabLayout.ViewPagerOnTabSelectedListener(view_pager))
+  }
+
+  companion object {
+
+    @JvmStatic
+    fun newInstance() = CollectionTabFragment()
+  }
+}
